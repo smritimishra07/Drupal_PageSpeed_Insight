e43db748168e8fcddb1a9f7d9fcbeccc8175b1d8
Smriti Mishra <smritimishra835@gmail.com>
2024-12-18 17:35:09 +0530

    script to fetch PageSpeed Insight Reports using API

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Load environment variables from .env
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
